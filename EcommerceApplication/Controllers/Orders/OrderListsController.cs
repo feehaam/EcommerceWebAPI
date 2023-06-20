8c7ea01acb1047298766fd37d67890d5d2b6b2e0
feehaam <mdfeeham@gmail.com>
2023-06-20 14:27:37 +0600

    All API complete

---
+++ EcommerceApplication/Controllers/Orders/OrderListsController.cs
@@ -0,0 +1,36 @@
+﻿using EcommerceApplication.IRepository.Orders;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApplication.Controllers.Orders
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderListsController : ControllerBase
+    {
+        private readonly IOrderRepoLists lists;
+
+        public OrderListsController(IOrderRepoLists lists)
+        {
+            this.lists = lists;
+        }
+
+        [HttpGet("/orders")]
+        public IActionResult Get()
+        {
+            return Ok(lists.GetAllOrders());
+        }
+
+        [HttpGet("/orders/{status}")]
+        public IActionResult Get(string status)
+        {
+            return Ok(lists.GetOrdersByStatus(status));
+        }
+
+        [HttpGet("/orders/{userId:int}")]
+        public IActionResult GetbyUID(int userId)
+        {
+            return Ok(lists.GetOrdersByUser(userId));
+        }
+    }
+}
--- EcommerceApplication/Services/Orders/OrderRepoLists.cs
+++ EcommerceApplication/Services/Orders/OrderRepoLists.cs
@@ -1,23 +1,52 @@
-﻿using EcommerceApplication.IRepository.Orders;
+﻿using EcommerceApplication.DBContext;
+using EcommerceApplication.IRepository.Orders;
 using EcommerceApplication.Models.Orders;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceApplication.Repository.Orders
 {
     public class OrderRepoLists : IOrderRepoLists
     {
+        private readonly DataContext _context;
+
+        public OrderRepoLists(DataContext context)
+        {
+            _context = context;
+        }
+
         public ICollection<Order> GetAllOrders()
         {
-            throw new NotImplementedException();
+            return _context.Orders
+                .Include(o => o.Products)
+                    .ThenInclude(p => p.Product)
+                .Include(o => o.DeliveryAddress)
+                .Include(o => o.PaymentStatus)
+                    .ThenInclude(ps => ps.Payments)
+                .ToList();
         }
 
         public ICollection<Order> GetOrdersByStatus(string status)
         {
-            throw new NotImplementedException();
+            return _context.Orders
+                .Where(o => o.DeliveryStatus.Contains(status))
+                .Include(o => o.Products)
+                    .ThenInclude(p => p.Product)
+                .Include(o => o.DeliveryAddress)
+                .Include(o => o.PaymentStatus)
+                    .ThenInclude(ps => ps.Payments)
+                .ToList();
         }
 
         public ICollection<Order> GetOrdersByUser(int UserId)
         {
-            throw new NotImplementedException();
+            return _context.Orders
+                .Where(o => o.UserId == UserId)
+                .Include(o => o.Products)
+                    .ThenInclude(p => p.Product)
+                .Include(o => o.DeliveryAddress)
+                .Include(o => o.PaymentStatus)
+                    .ThenInclude(ps => ps.Payments)
+                .ToList();
         }
     }
 }
